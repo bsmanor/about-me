8fe765b26f4678de828c216a0ced8241d3556163
bsmanor <bsmanor@gmail.com>
2020-06-15 01:43:17 +0300

    added cookies and fixed CSP headers to allow local host

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.append("Content-Security-Policy", "default-src 'self'");
+router.get('/', function(req, res) {
+  console.log(req.cookies.username);
+  res.append("Content-Security-Policy", "script-src 'self' 'unsafe-inline' https://code.jquery.com;");
   res.render('index', { title: 'Express' });
 });
 
